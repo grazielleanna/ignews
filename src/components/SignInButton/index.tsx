5d8f7ecc149735efb1d2337bb7a910183b46dead
Grazielle Conceição <grazielle@internit.com.br>
2022-01-03 18:54:55 -0300

    Realizando autenticação com next-auth e integrando criação de usuários ao faunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,30 @@
-import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi';
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#04D361" />
-            Grazielle Conceição
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
-    ) : (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#EBA417" />
-            Sign in with Github
-        </button>
-    )
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04D361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#EBA417" />
+      Sign in with Github
+    </button>
+  );
 }
